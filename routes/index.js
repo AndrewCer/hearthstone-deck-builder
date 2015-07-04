db9e28b413d35b8ac20c8c4fc303429e0c09a462
Andrew Cer <cer.andrew@gmail.com>
2015-07-04 16:12:18 -0600

    everything mostly working

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,8 @@ var db = require('monk')(process.env.MONGO_URI);
 var users = db.get('user');
 var userDecks = db.get('userdeck');
 var bcrypt = require('bcryptjs');
+var userNameValidator = require("../lib/validation.js").validUserName;
+var passwordValidator = require("../lib/validation.js").validPassword;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -46,20 +48,47 @@ router.post('/signup', function (req, res, next) {
   var userNameInput = req.body.user_name;
   var userPwInput = req.body.password;
   var userPwConfirm = req.body.pass_confirm;
-  if (userPwInput === userPwConfirm) {
-    var hash = bcrypt.hashSync(userPwInput, 8);
-    users.insert({userName: userNameInput, password: hash});
-    res.redirect('/');
+  var validUsernameArray = userNameValidator(userNameInput);
+  var validPasswordArray = passwordValidator(userPwInput);
+  if (validUsernameArray.length > 0 || validPasswordArray.length > 0) {
+    res.render('signup', {userName: userNameInput, usernameErrorArray: validUsernameArray,
+      pwErrorArray: validPasswordArray});
   }
   else {
-    res.render('signup', {userName: userNameInput, inputError: 'Passwords do not match'});
+    users.findOne({userName: userNameInput}, function (err, data) {
+      if (data) {
+        if (data.userName != userNameInput) {
+          if (userPwInput === userPwConfirm) {
+            var hash = bcrypt.hashSync(userPwInput, 8);
+            users.insert({userName: userNameInput, password: hash});
+            // res.cookie('currentUser', data.userName);
+            res.redirect('/');
+          }
+          else {
+            res.render('signup', {userName: userNameInput, inputError: 'Passwords do not match'});
+          }
+        }
+        else {
+          res.render('signup', {userName: userNameInput, inputError: 'That username already exists'});
+        }
+      }
+      else {
+        if (userPwInput === userPwConfirm) {
+          var hash = bcrypt.hashSync(userPwInput, 8);
+          users.insert({userName: userNameInput, password: hash});
+          res.redirect('/');
+        }
+        else {
+          res.render('signup', {userName: userNameInput, inputError: 'Passwords do not match'});
+        }
+      }
+    });
   }
 });
 
 router.post('/login', function (req, res, next) {
   var userNameInput = req.body.user_name;
   var userPwInput = req.body.password;
-  // var pwHash = bcrypt.compareSync(userPwInput, hash);
   users.findOne({userName: userNameInput}, function (err, data) {
     bcrypt.compare(userPwInput, data.password, function (err, answer) {
       if (answer === true) {
@@ -73,30 +102,62 @@ router.post('/login', function (req, res, next) {
   });
 });
 
+router.post('/logout', function (req, res, next) {
+  var userNameCookie = req.cookies.currentUser
+  res.clearCookie('currentUser');
+  res.redirect('/');
+});
+
 router.get('/user-cards/:id', function (req, res, next) {
   var userNameCookie = req.cookies.currentUser
   var cardInfoArray = [];
   userDecks.findOne({userName: userNameCookie}, function (err, data) {
-    // for (var i = 0; i < data.usersCards.length; i++) {
-    unirest.get("https://omgvamp-hearthstone-v1.p.mashape.com/cards/classes/" + data.cardClass[0])
-    .header("X-Mashape-Key", process.env.MASH_KEY)
-    .end(function (result) {
-      for (var i = 0; i < result.body.length; i++) {
-        for (var j = 0; j < data.usersCards.length; j++) {
-          if (result.body[i].cardId === data.usersCards[j]) {
-            cardInfoArray.push(result.body[i]);
+    if (data) {
+      unirest.get("https://omgvamp-hearthstone-v1.p.mashape.com/cards/classes/" + data.cardClass)
+      .header("X-Mashape-Key", process.env.MASH_KEY)
+      .end(function (result) {
+        for (var i = 0; i < result.body.length; i++) {
+          for (var j = 0; j < data.usersCards.length; j++) {
+            if (result.body[i].cardId === data.usersCards[j]) {
+              cardInfoArray.push(result.body[i]);
+            }
           }
         }
-      }
-      res.render('user-cards', {userName: userNameCookie, deckInfo: cardInfoArray});
-    });
+        res.render('user-cards', {userName: userNameCookie, deckInfo: cardInfoArray});
+      });
+    }
+    else {
+      res.render('user-cards', {userName: userNameCookie});
+    }
   });
 });
 
 router.post('/user-cards/:id', function (req, res, next) {
   var userNameCookie = req.cookies.currentUser;
+  // var cardClassObj = {};
+  // cardClassObj[req.body.playerClass[0]] = req.body.cardId;
+  // userDecks.findOne({userName: userNameCookie}, function (err, data) {
+  //   if (data) {
+  //     userDecks.update({userName: userNameCookie}, {$push: {usersCards: req.body.cardId,
+  //     cardClass: req.body.playerClass[0]}});
+  //     res.redirect('/user-cards/' + req.params.id);
+  //   }
+  //   else {
   userDecks.insert({userName: userNameCookie, usersCards: req.body.cardId,
-  cardClass: req.body.playerClass});
+  cardClass: req.body.playerClass[0]});
+  res.redirect('/user-cards/' + req.params.id);
+  //   }
+  // });
+
+});
+
+router.post('/remove-cards/:id', function (req, res, next) {
+  var userNameCookie = req.cookies.currentUser;
+  userDecks.findOne({userName: userNameCookie}, function (err, data) {
+    for (key in req.body) {
+      userDecks.update({usersCards: key}, { $pull: {usersCards: key}});
+    }
+  });
   res.redirect('/user-cards/' + req.params.id);
 });
 
